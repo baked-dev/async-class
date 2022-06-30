ef4ece5ce9dc906351e1205ca5816efc6d8c53d5
xhalfbakedguy <tristancilltseinleben@gmail.com>
2022-06-30 05:48:25 +0200

    updated types and build script

--- build.js
+++ build.js
@@ -1,5 +1,11 @@
 const { execSync } = require("child_process");
 const { build: esbuild } = require("esbuild");
+const { promises: { readFile, writeFile } } = require("fs");
+
+const copy = async (file, out) => {
+  const data = await readFile(file);
+  return Promise.all(out.map(async outFile => writeFile(outFile, data)));
+}
 
 const configs = [{
   entryPoints: ["src/index.ts"],
@@ -11,4 +17,8 @@ const configs = [{
   format: "cjs"
 }]
 
-Promise.all(configs.map(esbuild)).then(() => execSync("tsc"));
+Promise.all(configs.map(esbuild))
+  .then(() => execSync("tsc"))
+  .then(() => copy(
+    "dist/index.d.ts", ["dist/esm/index.d.ts", "dist/cjs/index.d.ts"]
+  ));
