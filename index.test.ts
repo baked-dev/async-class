a60e16d8df258f28467c4a5b659f9f10035a7d23
xhalfbakedguy <tristancilltseinleben@gmail.com>
2022-06-30 03:29:12 +0200

    added jest

---
+++ index.test.ts
@@ -0,0 +1,65 @@
+import { randomBytes } from "crypto";
+import { AwaitableClass } from "./";
+
+describe("AwaitableClass", () => {
+  class ExtendingClass extends AwaitableClass<[string?]> {
+    public test = "";
+
+    protected async init(input: string = "asd"): Promise<any> {
+      await new Promise((res) => setTimeout(res, 0));
+      this.test = input;
+    }
+
+    public get name() {
+      return this.test;
+    }
+
+    public set name(value: string) {
+      this.test = value;
+    }
+
+    public name2 = () => this.test;
+
+    public name3() {
+      return this.test;
+    }
+  }
+
+  it("is awaitable", async () => {
+    const instance = await new ExtendingClass();
+    expect(instance).toBeInstanceOf(ExtendingClass);
+    expect((instance as ExtendingClass).then).toEqual(undefined);
+  });
+
+  it("allows awaiting the same instance twice", async () => {
+    const awaitable = new ExtendingClass();
+    const instance0 = await awaitable;
+    const instance1 = await awaitable;
+    expect(instance0).toBe(instance1);
+  });
+
+  it("allows for getters", async () => {
+    const input = randomBytes(4).toString("hex");
+    const instance = await new ExtendingClass(input);
+    expect(instance.name).toEqual(input);
+  });
+
+  it("allows for setters", async () => {
+    const input = randomBytes(4).toString("hex");
+    const instance = await new ExtendingClass();
+    instance.name = input;
+    expect(instance.test).toEqual(input);
+  });
+
+  it("allows for property method access", async () => {
+    const input = randomBytes(4).toString("hex");
+    const instance = await new ExtendingClass(input);
+    expect(instance.name2()).toEqual(input);
+  });
+
+  it("allows for member method access", async () => {
+    const input = randomBytes(4).toString("hex");
+    const instance = await new ExtendingClass(input);
+    expect(instance.name3()).toEqual(input);
+  });
+});
