a19a02a89c456c974e7130f674c690a629e4ffd7
xhalfbakedguy <tristancilltseinleben@gmail.com>
2022-06-30 04:59:51 +0200

    added index.d.ts to type esm and cjs

---
+++ index.d.ts
@@ -0,0 +1,13 @@
+import "./dist/index";
+
+declare module "@baked/async-class/dist/esm" {
+  export * from "./dist/index";
+}
+
+declare module "@baked/async-class/dist/cjs" {
+  export * from "./dist/index";
+}
+
+declare module "@baked/async-class" {
+  export * from "./dist/index";
+}
